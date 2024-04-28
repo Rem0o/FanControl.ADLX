e690ab899b95d0b155ed6d208785fbe99529db67
Rem0o <remi.mercier.software@gmail.com>
2024-04-28 16:08:57 -0400

    Update ADLXPlugin.cs

--- ADLXPlugin.cs
+++ ADLXPlugin.cs
@@ -15,7 +15,7 @@ namespace FanControl.ADLX
         private IReadOnlyList<GPU> _gpus;
         private GPUTuningService _tuning;
         private PerformanceMonitor _perf;
-        private ManualFanTuning[] _fans;
+        private Dictionary<int, ManualFanTuning> _fans;
 
         private bool _initialized;
         private GPUMetricsProvider[] _metricsProviders;
@@ -34,7 +34,7 @@ namespace FanControl.ADLX
             {
                 _initialized = false;
 
-                _fans?.ToList().ForEach(x => x.Dispose());
+                _fans?.Values.ToList().ForEach(x => x.Dispose());
                 _perf?.Dispose();
                 _tuning?.Dispose();
                 _gpus?.ToList().ForEach(x => x.Dispose());
@@ -64,7 +64,7 @@ namespace FanControl.ADLX
                             Log($"Manual fan tuning for {g.Name} is not supported");
 
                         return supported;
-                    }).Select(_tuning.GetManualFanTuning).ToArray();
+                    }).ToDictionary(x => x.UniqueId, x => _tuning.GetManualFanTuning(x));
 
                     _metricsProviders = _gpus.Select(x => new GPUMetricsProvider(_perf, x)).ToArray();
                     _initialized = true;
@@ -84,7 +84,8 @@ namespace FanControl.ADLX
                 return;
             }
 
-            ADLXControl[] controls = _gpus.Zip(_fans, (gpu, fan) => new ADLXControl(gpu, fan)).ToArray();
+            ADLXControl[] controls = _gpus.Where(x => _fans.ContainsKey(x.UniqueId)).Select(x => new ADLXControl(x, _fans[x.UniqueId])).ToArray();
+
             ADLXFanSensor[] fanSensors = _gpus.Zip(_metricsProviders, (gpu, m) => new ADLXFanSensor(gpu, m)).ToArray();
             ADLXTemperatureSensor[] hotspots = _gpus.Zip(_metricsProviders, (gpu, m) => new ADLXTemperatureSensor("Hotspot", gpu, () => m.Current.GPUHotspotTemperature)).ToArray();
             ADLXTemperatureSensor[] gpuTemps = _gpus.Zip(_metricsProviders, (gpu, m) => new ADLXTemperatureSensor("GPU", gpu, () => m.Current.GPUTemperature)).ToArray();
